0c42adbfbec133e1202b863e53afde994756ef23
Ted Gould <ted@gould.cx>
2014-04-24 15:49:17 -0500

    Adding in a test framework for the memory database

---
+++ tests/item-memory-tests.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include "service/item-memory.hpp"
+
+struct MemoryItemTests : public ::testing::Test
+{
+	protected:
+		virtual void SetUp() {
+		}
+
+		virtual void TearDown() {
+		}
+};
+
+TEST_F(MemoryItemTests, BasicCreate) {
+	return;
+}
