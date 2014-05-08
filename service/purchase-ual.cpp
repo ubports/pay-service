2dd9ac633e77587d7857ccef9cad58748b606fe1
Ted Gould <ted@gould.cx>
2014-05-08 10:53:52 -0500

    Using UAL to start the application

--- service/purchase-ual.cpp
+++ service/purchase-ual.cpp
@@ -18,6 +18,9 @@
  */
 
 #include "purchase-ual.h"
+#include <thread>
+#include <upstart-app-launch.h>
+#include <gio/gio.h>
 
 namespace Purchase
 {
@@ -28,17 +31,104 @@ public:
     typedef std::shared_ptr<Item> Ptr;
 
     UalItem (std::string& in_appid, std::string& in_itemid) :
-        appid(in_appid), itemid(in_itemid)
+        appid(in_appid), itemid(in_itemid), ui_appid("gedit")
     {
+        /* TODO: ui_appid needs to be grabbed from the click hook */
     }
+
     virtual bool run (void)
     {
-        std::cout << "Running" << std::endl;
+        t = std::thread([this]()
+        {
+            /* Build up the context and loop for the async events and a place
+               for GDBus to send its events back to */
+            GMainContext* context = g_main_context_new();
+            GMainLoop* loop = g_main_loop_new(context, FALSE);
+
+            g_main_context_push_thread_default(context);
+
+            /* We're grabbing the bus to ensure we can get it, but also
+               to keep it connected for the lifecycle of this thread */
+            GDBusConnection* bus = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
+            if (bus == NULL)
+            {
+                /* TODO: Signal not purchased */
+                return;
+            }
+
+            upstart_app_launch_observer_add_app_stop(app_stop_static_helper, this);
+            upstart_app_launch_observer_add_app_failed(app_failed_static_helper, this);
+
+            /* Building a URL so that we can pass this information today without
+               using trusted helpers and setting environment vars */
+            /* TODO: Use trusted helpers */
+            std::string purchase_url = "purchase:///";
+            purchase_url += appid;
+            purchase_url += "/";
+            purchase_url += itemid;
+            const gchar* urls[2] = {0};
+            urls[0] = purchase_url.c_str();
+
+            if (!upstart_app_launch_start_application(ui_appid.c_str(), urls))
+            {
+                /* TODO: Signal not purchased */
+            }
+            else
+            {
+                g_main_loop_run(loop);
+            }
+
+            upstart_app_launch_observer_delete_app_stop(app_stop_static_helper, this);
+            upstart_app_launch_observer_delete_app_failed(app_failed_static_helper, this);
+
+            g_clear_object(&bus);
+            g_main_loop_unref(loop);
+            g_main_context_unref(context);
+        });
+
+        return true;
     };
 
 private:
     std::string appid;
     std::string itemid;
+    std::string ui_appid;
+    std::thread t;
+
+    static void app_stop_static_helper (const gchar* appid, gpointer user_data)
+    {
+        UalItem* notthis = static_cast<UalItem*>(user_data);
+        notthis->appStop(std::string(appid));
+    }
+
+    static void app_failed_static_helper (const gchar* appid, UpstartAppLaunchAppFailed failure_type, gpointer user_data)
+    {
+        /* we're not actually using the failure type, we don't care why */
+        UalItem* notthis = static_cast<UalItem*>(user_data);
+        notthis->appFailed(std::string(appid));
+    }
+
+    void appStop (std::string stop_appid)
+    {
+        if (stop_appid != appid)
+        {
+            return;
+        }
+
+        /* Set status to purchased */
+        /* Stop main loop */
+    }
+
+    void appFailed (std::string failed_appid)
+    {
+        if (failed_appid != appid)
+        {
+            return;
+        }
+
+        /* Set status to not purchased */
+        /* Stop main loop */
+    }
 };
 
 Item::Ptr
