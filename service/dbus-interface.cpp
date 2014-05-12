b4c6377e97574937e0077677a77dac0858aa8c57
Ted Gould <ted@gould.cx>
2014-05-12 11:53:44 -0500

    Put the bus in it's own thread and chill for a bit

--- service/dbus-interface.cpp
+++ service/dbus-interface.cpp
@@ -20,26 +20,92 @@
 #include "dbus-interface.h"
 
 #include <gio/gio.h>
+#include <thread>
 
 class DBusInterfaceImpl
 {
 public:
+    /* Allocated on main thread with init */
     Item::Store::Ptr items;
+    std::thread t;
+
+    /* Allocated on thread, and cleaned up there */
+    GMainLoop* loop;
+
+    /* Allocated on thread, cleaned up on main */
     GDBusConnection* bus;
 
+    /* Allocates a thread to do dbus work */
     DBusInterfaceImpl (const Item::Store::Ptr& in_items) : items(in_items)
     {
-        bus = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
+        t = std::thread([this]()
+        {
+            GMainContext* context = g_main_context_new();
+            loop = g_main_loop_new(context, FALSE);
+
+            g_main_context_push_thread_default(context);
+
+            g_bus_own_name(G_BUS_TYPE_SESSION,
+                           "com.canonical.pay",
+                           G_BUS_NAME_OWNER_FLAGS_NONE,
+                           busAcquired_staticHelper,
+                           nullptr,
+                           nameLost_staticHelper,
+                           this,
+                           nullptr /* free func for this */);
+
+            g_main_loop_run(loop);
+
+            g_clear_pointer(&loop, g_main_loop_unref);
+            g_clear_pointer(&context, g_main_context_unref);
+        });
     }
 
     ~DBusInterfaceImpl ()
     {
-        g_clear_object(&bus);
+        if (loop != nullptr)
+        {
+            g_main_loop_quit(loop);
+        }
+
+        if (t.joinable())
+        {
+            t.join();
+        }
     }
 
     GDBusConnection* getBus (void)
     {
         return bus;
+    }
+
+    /* Export objects into the bus before we get a name */
+    void busAcquired ()
+    {
+
+    }
+
+    /* We only get this on errors, so we need to throw the exception and
+       be done with it. */
+    void nameLost ()
+    {
+        throw std::runtime_error("Unable to get dbus name: 'com.canonical.pay'");
+    }
+
+
+    /**************************************
+     * Static Helpers, C language binding
+     **************************************/
+    static void busAcquired_staticHelper (GDBusConnection* bus, const gchar* name, gpointer user_data)
+    {
+        DBusInterfaceImpl* notthis = static_cast<DBusInterfaceImpl*>(user_data);
+        notthis->busAcquired();
+    }
+
+    static void nameLost_staticHelper (GDBusConnection* bus, const gchar* name, gpointer user_data)
+    {
+        DBusInterfaceImpl* notthis = static_cast<DBusInterfaceImpl*>(user_data);
+        notthis->nameLost();
     }
 };
 
