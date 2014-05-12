e7dc854bec4776d6cc7650745a023b89c23425c1
Ted Gould <ted@gould.cx>
2014-05-12 10:58:33 -0500

    Switch out dbus-cpp

--- service/dbus-interface.cpp
+++ service/dbus-interface.cpp
@@ -17,86 +17,41 @@
  *   Ted Gould <ted.gould@canonical.com>
  */
 
-#include <core/dbus/announcer.h>
-#include <core/dbus/service.h>
-#include <core/dbus/skeleton.h>
-#include <core/dbus/stub.h>
-#include <vector>
-
 #include "dbus-interface.h"
 
-class ApplicationsSkeleton : public core::dbus::Skeleton<DBusInterface::IApplications>
+#include <gio/gio.h>
+
+class DBusInterfaceImpl
 {
 public:
-    ApplicationsSkeleton(const core::dbus::Bus::Ptr& bus) : core::dbus::Skeleton<DBusInterface::IApplications>(bus),
-        object(access_service()->add_object_for_path(core::dbus::types::ObjectPath("/com/canonical/pay")))
-    {
-        object->install_method_handler<DBusInterface::IApplications::GetApplications>(std::bind(
-                                                                                          &ApplicationsSkeleton::handle_get_applications, this, std::placeholders::_1));
-    }
-
-private:
-    void handle_get_applications (const core::dbus::Message::Ptr& msg)
-    {
-        auto out = get_applications();
-        auto reply = core::dbus::Message::make_method_return(msg);
-        reply->writer() << out;
-
-        access_bus()->send(reply);
-    }
-
-    core::dbus::Object::Ptr object;
-};
-
-class Applications : public ApplicationsSkeleton
-{
-public:
-    typedef std::shared_ptr<Applications> Ptr;
-
-    Applications (const core::dbus::Bus::Ptr& bus, Item::Store::Ptr in_items) :
-        ApplicationsSkeleton(bus),
-        items(in_items) { }
-
-    std::vector<core::dbus::types::ObjectPath> get_applications ()
-    {
-        std::vector<core::dbus::types::ObjectPath> retval;
-        auto applist = items->listApplications();
-
-        for (auto app : applist)
-        {
-            /* TODO: encode into valid path charset */
-            core::dbus::types::ObjectPath op("/com/canonical/pay/application/" + app);
-            retval.push_back(op);
-        }
-
-        return retval;
-    }
-
-private:
     Item::Store::Ptr items;
+    GDBusConnection* bus;
+
+    DBusInterfaceImpl (const Item::Store::Ptr& in_items) : items(in_items)
+    {
+        bus = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
+    }
+
+    ~DBusInterfaceImpl ()
+    {
+        g_clear_object(&bus);
+    }
+
+    GDBusConnection* getBus (void)
+    {
+        return bus;
+    }
 };
 
-DBusInterface::DBusInterface (core::dbus::Bus::Ptr& in_bus, Item::Store::Ptr in_items) :
-    bus(in_bus),
-    items(in_items),
-    base(core::dbus::announce_service_on_bus<DBusInterface::IApplications, Applications>(in_bus, in_items))
+DBusInterface::DBusInterface (const Item::Store::Ptr& in_items)
 {
-    items->itemChanged.connect([this](std::string& app, std::string& item, Item::Item::Status status)
-    {
-        std::string app_encode = encodePath(app);
-        std::string item_encode = encodePath(item);
+    impl = std::make_shared<DBusInterfaceImpl>(in_items);
+}
 
-        auto signal = core::dbus::Message::make_signal("/com/canonical/pay/application/" + app_encode + "/" + item_encode,
-                                                       "com.canonical.pay.item",
-                                                       "statusChanged");
-
-        /* TODO: Get status string from the obj */
-        std::string strstatus("status");
-        signal->writer() << strstatus;
-        bus->send(signal);
-    });
-
-    return;
+bool
+DBusInterface::busStatus ()
+{
+    return impl->getBus() != nullptr;
 }
 
 std::string
@@ -162,3 +117,4 @@ DBusInterface::decodePath (const std::string& input)
 
     return output;
 }
+
--- service/dbus-interface.h
+++ service/dbus-interface.h
@@ -17,61 +17,27 @@
  *   Ted Gould <ted.gould@canonical.com>
  */
 
-#include <core/dbus/bus.h>
-#include <core/dbus/service.h>
 #include "item-interface.h"
+#include <memory>
 
 #ifndef DBUS_INTERFACE_HPP__
 #define DBUS_INTERFACE_HPP__ 1
 
-class Applications;
+class DBusInterfaceImpl;
 
 class DBusInterface {
-
 public:
-	class IApplications
-	{
-	public:
-		struct GetApplications
-		{
-			typedef IApplications Interface;
-			inline static const std::string & name () {
-				static const std::string s {"GetApplications"};
-				return s;
-			}
-			inline static const std::chrono::milliseconds default_timeout() {
-				return std::chrono::seconds{1};
-			}
-		};
-
-	public:
-		virtual ~IApplications() = default;
-		virtual std::vector<core::dbus::types::ObjectPath> get_applications (void) = 0;
-	};
-
-
-public:
-	DBusInterface (core::dbus::Bus::Ptr& in_bus, Item::Store::Ptr in_items);
+	DBusInterface (const Item::Store::Ptr& in_items);
 	~DBusInterface () { };
+
+	bool busStatus ();
 
 	static std::string encodePath (const std::string& input);
 	static std::string decodePath (const std::string& input);
 
 private:
-	Item::Store::Ptr items;
-	core::dbus::Bus::Ptr bus;
-	std::shared_ptr<Applications> base;
+	std::shared_ptr<DBusInterfaceImpl> impl;
 };
-
-namespace core { namespace dbus { namespace traits {
-template<> struct Service<DBusInterface::IApplications>
-{
-	inline static const std::string& interface_name() {
-		static const std::string s {"com.canonical.pay"};
-		return s;
-	}
-};
-}}} /* namespaces */
 
 
 #endif // DBUS_INTERFACE_HPP__
