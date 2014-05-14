ab57e7e7a455c7fd77694e17ce2a1455c8a149a2
Ted Gould <ted@gould.cx>
2014-05-14 13:42:34 -0500

    Drop GLib from header

--- libpay/pay-package.h
+++ libpay/pay-package.h
@@ -1,5 +1,4 @@
 
-#include <glib.h>
 
 typedef struct _PayPackage PayPackage;
 typedef enum {
@@ -9,14 +8,14 @@ typedef enum {
 	PAY_PACKAGE_ITEM_STATUS_PURCHASING,
 	PAY_PACKAGE_ITEM_STATUS_NOT_PURCHASED
 } PayPackageItemStatus;
-typedef void (*PayPackageItemObserver) (PayPackage * package, const gchar * itemid, PayPackageItemStatus status);
+typedef void (*PayPackageItemObserver) (PayPackage * package, const char * itemid, PayPackageItemStatus status);
 
-PayPackage * pay_package_create (const gchar * package_name);
+PayPackage * pay_package_create (const char * package_name);
 void pay_package_delete (PayPackage * package);
-PayPackageItemStatus pay_package_item_status (PayPackage * package, const gchar * itemid);
+PayPackageItemStatus pay_package_item_status (PayPackage * package, const char * itemid);
 gboolean pay_package_item_observer_add (PayPackage * package, PayPackageItemObserver observer, gpointer user_data);
 gboolean pay_package_item_observer_delete (PayPackage * package, PayPackageItemObserver observer, gpointer user_data);
 
-gboolean pay_package_item_start_verification (PayPackage * package, const gchar * itemid);
-gboolean pay_package_item_start_purchase (PayPackage * package, const gchar * itemid);
+gboolean pay_package_item_start_verification (PayPackage * package, const char * itemid);
+gboolean pay_package_item_start_purchase (PayPackage * package, const char * itemid);
 
