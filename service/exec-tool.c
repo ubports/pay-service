798693dda62fde1edcc160fa0e5a8ff4435d7b2c
Ted Gould <ted@gould.cx>
2014-07-24 12:37:40 -0500

    Set the APP_DIR with the libclick pkgdir

--- service/exec-tool.c
+++ service/exec-tool.c
@@ -18,6 +18,8 @@
  */
 
 #include <glib.h>
+#include <click.h>
+#include <ubuntu-app-launch.h>
 
 gchar *
 build_exec_envvar (const gchar * appid)
@@ -95,6 +97,53 @@ build_uri_envvar(const gchar * appuris, gchar ** euri, gchar ** esocket)
 	return TRUE;
 }
 
+gchar *
+build_dir_envvar (const gchar * appid)
+{
+	GError * error = NULL;
+	gchar * package = NULL;
+	/* 'Parse' the App ID */
+	if (!ubuntu_app_launch_app_id_parse(appid, &package, NULL, NULL)) {
+		g_warning("Unable to parse App ID: '%s'", appid);
+		return NULL;
+	}
+
+	/* Check click to find out where the files are */
+	ClickDB * db = click_db_new();
+	/* If TEST_CLICK_DB is unset, this reads the system database. */
+	click_db_read(db, g_getenv("TEST_CLICK_DB"), &error);
+	if (error != NULL) {
+		g_warning("Unable to read Click database: %s", error->message);
+		g_error_free(error);
+		g_free(package);
+		return NULL;
+	}
+	/* If TEST_CLICK_USER is unset, this uses the current user name. */
+	ClickUser * user = click_user_new_for_user(db, g_getenv("TEST_CLICK_USER"), &error);
+	if (error != NULL) {
+		g_warning("Unable to read Click database: %s", error->message);
+		g_error_free(error);
+		g_free(package);
+		g_object_unref(db);
+		return NULL;
+	}
+	gchar * pkgdir = click_user_get_path(user, package, &error);
+
+	g_object_unref(user);
+	g_object_unref(db);
+	g_free(package);
+
+	if (error != NULL) {
+		g_warning("Unable to get the Click package directory for %s: %s", package, error->message);
+		g_error_free(error);
+		return NULL;
+	}
+
+	gchar * envvar = g_strdup_printf("APP_DIR=%s", pkgdir);
+	g_free(pkgdir);
+	return envvar;
+}
+
 int
 main (int argc, char * argv[])
 {
@@ -128,14 +177,18 @@ main (int argc, char * argv[])
 		return -1;
 	}
 
+	gchar * envdir = build_dir_envvar(appid);
+	/* envdir might be NULL if not a click */
+
 	/* Execute the setting of the variables! */
 
-	gchar * initctlargv[6] = {
+	gchar * initctlargv[7] = {
 		"initctl",
 		"set-env",
 		envexec,
 		envuri,
 		envsocket,
+		envdir,
 		NULL
 	};
 
