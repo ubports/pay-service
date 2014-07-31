3035e9664d8d4f4b582b6d1c915b04215b376700
Alejandro J. Cura <alecu@canonical.com>
2014-07-31 16:33:55 -0300

    Enable purchases in the script to setup the staging environ

--- tests/setup-staging.sh
+++ tests/setup-staging.sh
@@ -10,6 +10,7 @@ URL_PACKAGE_INFO=https://search.apps.staging.ubuntu.com/api/v1/package/
 ACCOUNT_CREDS_URL=https://login.staging.ubuntu.com/api/v2/tokens/oauth
 ADD_PAYMENT_URL=https://sc.staging.ubuntu.com/api/2.0/click/paymentmethods/add/
 U1_SEARCH_BASE_URL=https://search.apps.staging.ubuntu.com/
+CLICK_STORE_ENABLE_PURCHASES=1
 
 echo "Setting up upstart environment variables"
 
@@ -20,6 +21,7 @@ echo "Setting up upstart environment variables"
 /sbin/initctl set-env --global ACCOUNT_CREDS_URL=$ACCOUNT_CREDS_URL
 /sbin/initctl set-env --global ADD_PAYMENT_URL=$ADD_PAYMENT_URL
 /sbin/initctl set-env --global U1_SEARCH_BASE_URL=$U1_SEARCH_BASE_URL
+/sbin/initctl set-env --global CLICK_STORE_ENABLE_PURCHASES=$CLICK_STORE_ENABLE_PURCHASES
 
 echo "Setting up dbus environment variables"
 
@@ -27,7 +29,7 @@ gdbus call --session \
 	--dest org.freedesktop.DBus \
 	--object-path / \
 	--method org.freedesktop.DBus.UpdateActivationEnvironment \
-	"[{'SSO_AUTH_BASE_URL', '$SSO_AUTH_BASE_URL'}, {'SSO_UONE_BASE_URL', '$SSO_UONE_BASE_URL'}, {'PAY_BASE_URL', '$PAY_BASE_URL'}, {'URL_PACKAGE_INFO', '$URL_PACKAGE_INFO'}, {'ACCOUNT_CREDS_URL', '$ACCOUNT_CREDS_URL'}, {'ADD_PAYMENT_URL', '$ADD_PAYMENT_URL'}, {'U1_SEARCH_BASE_URL', '$U1_SEARCH_BASE_URL'}]"
+	"[{'SSO_AUTH_BASE_URL', '$SSO_AUTH_BASE_URL'}, {'SSO_UONE_BASE_URL', '$SSO_UONE_BASE_URL'}, {'PAY_BASE_URL', '$PAY_BASE_URL'}, {'URL_PACKAGE_INFO', '$URL_PACKAGE_INFO'}, {'ACCOUNT_CREDS_URL', '$ACCOUNT_CREDS_URL'}, {'ADD_PAYMENT_URL', '$ADD_PAYMENT_URL'}, {'U1_SEARCH_BASE_URL', '$U1_SEARCH_BASE_URL'}, {'CLICK_STORE_ENABLE_PURCHASES', '$CLICK_STORE_ENABLE_PURCHASES'}]"
 
 echo "Restarting scope registry"
 /sbin/restart scope-registry
