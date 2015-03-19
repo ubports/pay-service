e9a6efe57d1616813ac82e9883de44de716a7936
Ted Gould <ted@gould.cx>
2015-03-19 09:36:41 -0500

    Style cleanup

--- service/glib-thread.h
+++ service/glib-thread.h
@@ -19,20 +19,22 @@
 
 #include <gio/gio.h>
 
-namespace GLib {
+namespace GLib
+{
 
-class ContextThread {
-	std::thread _thread;
-	std::shared_ptr<GMainContext> _context;
-	std::shared_ptr<GMainLoop> _loop;
-	std::shared_ptr<GCancellable> _cancel;
+class ContextThread
+{
+    std::thread _thread;
+    std::shared_ptr<GMainContext> _context;
+    std::shared_ptr<GMainLoop> _loop;
+    std::shared_ptr<GCancellable> _cancel;
 
 public:
-	ContextThread (std::function<void(void)> beforeLoop = [](void){}, std::function<void(void)> afterLoop = []{})
-		: _context(nullptr)
-		, _loop(nullptr)
-	{
-		_cancel = std::shared_ptr<GCancellable>(g_cancellable_new(), [](GCancellable * cancel)
+    ContextThread (std::function<void(void)> beforeLoop = [](void) {}, std::function<void(void)> afterLoop = [] {})
+        : _context(nullptr)
+        , _loop(nullptr)
+    {
+        _cancel = std::shared_ptr<GCancellable>(g_cancellable_new(), [](GCancellable * cancel)
         {
             if (cancel != nullptr)
             {
@@ -40,12 +42,13 @@ public:
                 g_object_unref(cancel);
             }
         });
-		std::promise<std::pair<std::shared_ptr<GMainContext>, std::shared_ptr<GMainLoop>>> context_promise;
+        std::promise<std::pair<std::shared_ptr<GMainContext>, std::shared_ptr<GMainLoop>>> context_promise;
 
-		/* NOTE: We copy afterLoop but reference beforeLoop. We're blocking so we
-		   know that beforeLoop will stay valid long enough, but we can't say the
-		   same for afterLoop */
-		_thread = std::thread([&context_promise, &beforeLoop, afterLoop, this](void) -> void {
+        /* NOTE: We copy afterLoop but reference beforeLoop. We're blocking so we
+           know that beforeLoop will stay valid long enough, but we can't say the
+           same for afterLoop */
+        _thread = std::thread([&context_promise, &beforeLoop, afterLoop, this](void) -> void
+        {
             /* Build up the context and loop for the async events and a place
                for GDBus to send its events back to */
             auto context = std::shared_ptr<GMainContext>(g_main_context_new(), [](GMainContext * context)
@@ -65,102 +68,115 @@ public:
 
             g_main_context_push_thread_default(context.get());
 
-			/* Free's the constructor to continue */
-			auto pair = std::pair<std::shared_ptr<GMainContext>, std::shared_ptr<GMainLoop>>(context, loop);
-			context_promise.set_value(pair);
+            /* Free's the constructor to continue */
+            auto pair = std::pair<std::shared_ptr<GMainContext>, std::shared_ptr<GMainLoop>>(context, loop);
+            context_promise.set_value(pair);
 
-			beforeLoop();
+            beforeLoop();
 
             if (!g_cancellable_is_cancelled(_cancel.get()))
             {
                 g_main_loop_run(loop.get());
             }
 
-			afterLoop();
-		});
+            afterLoop();
+        });
 
-		/* We need to have the context and the mainloop ready before
-		   other functions on this object can work properly. So we wait
-		   for them and set them on this thread. */
-		auto context_future = context_promise.get_future();
-		context_future.wait();
-		auto context_value = context_future.get();
+        /* We need to have the context and the mainloop ready before
+           other functions on this object can work properly. So we wait
+           for them and set them on this thread. */
+        auto context_future = context_promise.get_future();
+        context_future.wait();
+        auto context_value = context_future.get();
 
-		_context = context_value.first;
-		_loop = context_value.second;
+        _context = context_value.first;
+        _loop = context_value.second;
 
-		if (_context == nullptr || _loop == nullptr) {
-			throw std::runtime_error("Unable to create GLib Thread");
-		}
-	}
+        if (_context == nullptr || _loop == nullptr)
+        {
+            throw std::runtime_error("Unable to create GLib Thread");
+        }
+    }
 
-	~ContextThread (void)
-	{
-		quit();
+    ~ContextThread (void)
+    {
+        quit();
 
-		if (_thread.joinable())
-			_thread.join();
-	}
+        if (_thread.joinable())
+        {
+            _thread.join();
+        }
+    }
 
-	void quit (void) {
-		g_cancellable_cancel(_cancel.get()); /* Force the cancellation on ongoing tasks */
-		if (_loop != nullptr)
-			g_main_loop_quit(_loop.get()); /* Quit the loop */
-	}
+    void quit (void)
+    {
+        g_cancellable_cancel(_cancel.get()); /* Force the cancellation on ongoing tasks */
+        if (_loop != nullptr)
+        {
+            g_main_loop_quit(_loop.get());    /* Quit the loop */
+        }
+    }
 
-	bool isCancelled (void) {
-		return g_cancellable_is_cancelled(_cancel.get()) == TRUE;
-	}
+    bool isCancelled (void)
+    {
+        return g_cancellable_is_cancelled(_cancel.get()) == TRUE;
+    }
 
-	template<typename T> auto executeOnThread (std::function<T(void)> work) -> T {
-		if (isCancelled()) {
-			throw std::runtime_error("Trying to execute work on a GLib thread that is shutting down.");
-		}
+    template<typename T> auto executeOnThread (std::function<T(void)> work) -> T
+    {
+        if (isCancelled())
+        {
+            throw std::runtime_error("Trying to execute work on a GLib thread that is shutting down.");
+        }
 
-		std::promise<T> promise;
-		std::function<gboolean(void)> magicFunc = [&promise, &work] (void) -> gboolean {
-			promise.set_value(work());
-			return G_SOURCE_REMOVE;
-		};
+        std::promise<T> promise;
+        std::function<gboolean(void)> magicFunc = [&promise, &work] (void) -> gboolean {
+            promise.set_value(work());
+            return G_SOURCE_REMOVE;
+        };
 
-		auto source = g_idle_source_new();
-		g_source_set_callback(source,
-		[](gpointer data) -> gboolean {
-			std::function<gboolean(void)> *magic = reinterpret_cast<std::function<gboolean(void)> *>(data);
-			return (*magic)();
-		}, &magicFunc, nullptr);
+        auto source = g_idle_source_new();
+        g_source_set_callback(source,
+        [](gpointer data) -> gboolean {
+            std::function<gboolean(void)>* magic = reinterpret_cast<std::function<gboolean(void)> *>(data);
+            return (*magic)();
+        }, &magicFunc, nullptr);
 
-		g_source_attach(source, _context.get());
-		g_source_unref(source);
+        g_source_attach(source, _context.get());
+        g_source_unref(source);
 
-		auto future = promise.get_future();
-		future.wait();
-		return future.get();
-	}
+        auto future = promise.get_future();
+        future.wait();
+        return future.get();
+    }
 
-	void executeOnThread (std::function<void(void)> work) {
-		if (isCancelled()) {
-			throw std::runtime_error("Trying to execute work on a GLib thread that is shutting down.");
-		}
+    void executeOnThread (std::function<void(void)> work)
+    {
+        if (isCancelled())
+        {
+            throw std::runtime_error("Trying to execute work on a GLib thread that is shutting down.");
+        }
 
-		/* The reason why this case is different is that we're not waiting on
-		   the return so we need to copy the function object */
-		std::function<void(void)> *heapWork = new std::function<void(void)>(work);
+        /* The reason why this case is different is that we're not waiting on
+           the return so we need to copy the function object */
+        std::function<void(void)>* heapWork = new std::function<void(void)>(work);
 
-		auto source = g_idle_source_new();
-		g_source_set_callback(source,
-		[](gpointer data) -> gboolean {
-			std::function<void(void)> *heapWork = reinterpret_cast<std::function<void(void)> *>(data);
-			(*heapWork)();
-			return G_SOURCE_REMOVE;
-		}, heapWork,
-		[](gpointer data) { 
-			std::function<void(void)> *heapWork = reinterpret_cast<std::function<void(void)> *>(data);
-			delete heapWork;
-		});
+        auto source = g_idle_source_new();
+        g_source_set_callback(source,
+                              [](gpointer data) -> gboolean
+        {
+            std::function<void(void)>* heapWork = reinterpret_cast<std::function<void(void)> *>(data);
+            (*heapWork)();
+            return G_SOURCE_REMOVE;
+        }, heapWork,
+        [](gpointer data)
+        {
+            std::function<void(void)>* heapWork = reinterpret_cast<std::function<void(void)> *>(data);
+            delete heapWork;
+        });
 
-		g_source_attach(source, _context.get());
-		g_source_unref(source);
-	}
+        g_source_attach(source, _context.get());
+        g_source_unref(source);
+    }
 };
 }
