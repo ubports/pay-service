c414575edd636366ddc96979dbed8616c4be3fa1
Ted Gould <ted@gould.cx>
2015-05-06 13:56:20 -0500

    Remove all the demangler stuff

--- service/exec-tool.c
+++ service/exec-tool.c
@@ -22,7 +22,7 @@
 #include <ubuntu-app-launch.h>
 
 gchar *
-build_exec_envvar (const gchar * appid)
+build_exec (const gchar * appid)
 {
 	gchar * appid_desktop = g_strdup_printf("%s.desktop", appid);
 	gchar * desktopfilepath = g_build_filename(g_get_user_cache_dir(), "pay-service", "pay-ui", appid_desktop, NULL);
@@ -57,91 +57,7 @@ build_exec_envvar (const gchar * appid)
 	gchar * exec = g_key_file_get_string(keyfile, "Desktop Entry", "Exec", NULL);
 	g_key_file_free(keyfile);
 
-	gchar * prepend = g_strdup_printf("%s %s", SOCKET_TOOL, exec);
-	g_free(exec);
-	g_debug("Final Exec line: %s", prepend);
-
-	gchar * envvar = g_strdup_printf("APP_EXEC=%s", prepend);
-	g_free(prepend);
-
-	return envvar;
-}
-
-gboolean
-build_uri_envvar(const gchar * appuris, gchar ** euri, gchar ** esocket)
-{
-	gint argc;
-	gchar ** argv;
-	GError * error = NULL;
-
-	g_shell_parse_argv(appuris, &argc, &argv, &error);
-	if (error != NULL) {
-		g_critical("Unable to parse URIs '%s': %s", appuris, error->message);
-		g_error_free(error);
-		return FALSE;
-	}
-
-	if (argc != 2) {
-		g_critical("We should be getting 2 entries from '%s' but got %d", appuris, argc);
-		g_strfreev(argv);
-		return FALSE;
-	}
-
-	*esocket = g_strdup_printf("PAY_SERVICE_MIR_SOCKET=%s", argv[0]);
-	gchar * quoted = g_shell_quote(argv[1]);
-	*euri = g_strdup_printf("APP_URIS=%s", quoted);
-	
-	g_free(quoted);
-	g_strfreev(argv);
-
-	return TRUE;
-}
-
-gchar *
-build_dir_envvar (const gchar * appid)
-{
-	GError * error = NULL;
-	gchar * package = NULL;
-	/* 'Parse' the App ID */
-	if (!ubuntu_app_launch_app_id_parse(appid, &package, NULL, NULL)) {
-		g_warning("Unable to parse App ID: '%s'", appid);
-		return NULL;
-	}
-
-	/* Check click to find out where the files are */
-	ClickDB * db = click_db_new();
-	/* If TEST_CLICK_DB is unset, this reads the system database. */
-	click_db_read(db, g_getenv("TEST_CLICK_DB"), &error);
-	if (error != NULL) {
-		g_warning("Unable to read Click database: %s", error->message);
-		g_error_free(error);
-		g_free(package);
-		return NULL;
-	}
-	/* If TEST_CLICK_USER is unset, this uses the current user name. */
-	ClickUser * user = click_user_new_for_user(db, g_getenv("TEST_CLICK_USER"), &error);
-	if (error != NULL) {
-		g_warning("Unable to read Click database: %s", error->message);
-		g_error_free(error);
-		g_free(package);
-		g_object_unref(db);
-		return NULL;
-	}
-	gchar * pkgdir = click_user_get_path(user, package, &error);
-
-	g_object_unref(user);
-	g_object_unref(db);
-	g_free(package);
-
-	if (error != NULL) {
-		g_warning("Unable to get the Click package directory for %s: %s", package, error->message);
-		g_error_free(error);
-		return NULL;
-	}
-
-	gchar * envvar = g_strdup_printf("APP_DIR=%s", pkgdir);
-	g_free(pkgdir);
-	return envvar;
+	return exec;
 }
 
 int
@@ -156,64 +72,13 @@ main (int argc, char * argv[])
 		return -1;
 	}
 
-	gchar * envexec = build_exec_envvar(appid);
-	if (envexec == NULL) {
+	gchar * exec = build_exec(appid);
+	if (exec == NULL) {
 		return -1;
 	}
 
-	/* Build up our socket name URL */
-	const gchar * appuris = g_getenv("APP_URIS");
-	if (appuris == NULL) {
-		g_error("Environment variable 'APP_URIS' required");
-		g_free(envexec);
-		return -1;
-	}
+	ubuntu_app_launch_helper_set_exec(exec);
+	g_free(exec);
 
-	gchar * envuri = NULL;
-	gchar * envsocket = NULL;
-
-	if (!build_uri_envvar(appuris, &envuri, &envsocket)) {
-		g_free(envexec);
-		return -1;
-	}
-
-	gchar * envdir = build_dir_envvar(appid);
-	/* envdir might be NULL if not a click */
-
-	/* Execute the setting of the variables! */
-
-	gchar * initctlargv[7] = {
-		"initctl",
-		"set-env",
-		envexec,
-		envuri,
-		envsocket,
-		envdir,
-		NULL
-	};
-
-	g_spawn_sync(
-		NULL, /* pwd */
-		initctlargv,
-		NULL, /* env */
-		G_SPAWN_SEARCH_PATH,
-		NULL, /* child setup */
-		NULL, /* user data ^ */
-		NULL, /* stdout */
-		NULL, /* stderr */
-		NULL, /* return code */
-		&error
-	);
-
-	g_free(envexec);
-	g_free(envuri);
-	g_free(envsocket);
-
-	if (error == NULL) {
-		return 0;
-	} else {
-		g_error("Unable to spawn 'initctl': %s", error->message);
-		g_error_free(error);
-		return -1;
-	}
+	return 0;
 }
